dfe07e608c7ad6c899408da2e1c1de1419bde15a
Matthias Panny <matthias.panny@mci.edu>
2024-05-10 16:13:08 +0200

    Added colored printing and included autocalls in output of functions()

--- calc_init.py
+++ calc_init.py
@@ -29,14 +29,14 @@ def workspace():
     List all variables in the current active python shell
     """
     tmp = globals().copy()
-    [print(F"{k}:\t{v}\ttype={type(v)}") for k, v in tmp.items() if not k.startswith('_') and k!='tmp' and k!='In' and k!='Out' and not callable(v) and not isinstance(v, ModuleType)]
+    [print(F"\x1b[36m{k}\x1b[0m:\t{v}\ttype={type(v)}") for k, v in tmp.items() if not k.startswith('_') and k!='tmp' and k!='In' and k!='Out' and not callable(v) and not isinstance(v, ModuleType)]
 
 def modules():
     """
     List all modules in the current active python shell
     """
     tmp = globals().copy()
-    [print(F"{k}:\t{v}\ttype={type(v)}") for k, v in tmp.items() if isinstance(v, ModuleType) and not k.startswith('_')]
+    [print(F"\x1b[36m{k}\x1b[0m:\t{v}\ttype={type(v)}") for k, v in tmp.items() if isinstance(v, ModuleType) and not k.startswith('_')]
 
 def functions():
     """
@@ -45,11 +45,18 @@ def functions():
     not_print = ["open"]
     tmp = globals().copy()
     for k, v in tmp.items():
-        if not k.startswith('_') and isinstance(v, FunctionType) and not k in not_print:
+        if not k.startswith('_') and (isinstance(v, FunctionType) or isinstance(v, IPyAutocall)) and not k in not_print:
             doc_string = v.__doc__.strip() if v.__doc__ else "No docstring available"
-            print(F"{k}:\t{doc_string}")
+            doc_string = doc_string.replace('\n', " ").replace("\t", " ")
+            doc_string = " ".join(doc_string.split()) #remove multiple spaces without regex
+
+            #https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
+            print(F"\x1b[36m{k}\x1b[0m:\t{doc_string}")
 
 class ClearAutocall(IPyAutocall):
+    """
+    Autocallable object to clear the terminal screen
+    """
     rewrite = False
     
     def __call__(self):
